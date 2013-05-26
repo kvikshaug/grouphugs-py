43ca80bcf9ee3102a7f831dc6edc4d9d1a3bcb6e
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2013-05-26 23:27:37 +0200

    Database mixin class

--- modules/logger.py
+++ modules/logger.py
@@ -1,21 +1,16 @@
-import sqlalchemy as sa
-import sqlalchemy.dialects.postgresql as pg
-from sqlalchemy.ext.mutable import MutableDict
-
 import logging
 import logging.config
 
-from modules.base import Base
+from modules.database import Database
 
-class Module():
+
+class Module(Database):
     def __init__(self, bot):
+        super().__init__()
         self.bot = bot
         self.bot.events.on_chanmsg += self.on_chanmsg
         self.log = logging.getLogger(__name__)
-        self.table = sa.Table('logs', Base.metadata,
-                              sa.Column('id', sa.Integer, primary_key=True),
-                              sa.Column('data', MutableDict.as_mutable(pg.HSTORE)))
-        self.engine = sa.create_engine(self.bot.options['database_uri'])
+        self.table = self.hstore_table_for('logs')
 
     def on_chanmsg(self, sender, channel, message):
         msg = {'sender': '{0}!{1}@{2}'.format(*sender), 'channel': channel, 'message': message}
