b6f2ffefaf248a19426be65f426a43eb7f8c4202
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2013-06-05 01:57:01 +0200

    bootstrap.sh - the server side part of deployment
    * creates a virtualenv and installs all dependencies
    * symlinks config.py
    * replaces 'current' and 'previous' symlinks

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+cd "$(dirname $0)"
+
+virtualenv --python=python3 --distribute env
+source env/bin/activate
+pip install -r requirements.txt
+
+ln -s ~/etc/config.py config.py
+
+release=$(pwd)
+cd ~
+if [[ -f current ]]; then
+    mv -f current previous
+fi
+ln -s "$release" current
+
